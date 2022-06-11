a1e73199e2950659b52f8195295a42d5cad2b51f
natnael <natiyeshi16@.com>
2022-06-11 16:46:26 +0300

    seacond

--- js.js
+++ js.js
@@ -1,19 +1,23 @@
 
-main1 = (pointer,num) =>{
-    pointer.style.display = "none"
-    nav_hover = document.querySelector(".nav-hover"+num)
-    nav_hover.style.display = "block"
+// main1 = (pointer,num) =>{
+//     pointer.style.display = "none"
+//     nav_hover = document.querySelector(".nav-hover"+num)
+//     nav_hover.style.display = "block"
+// }
+// main2 = (pointer,num) =>{
+//     pointer.style.display = "block"
+//     nav_hover = document.querySelector(".nav-hover"+num)
+//     nav_hover.style.display = "none"
+// }
+
+const front_img = document.querySelector(".front-img")
+front_img.style.position = "relative"
+console.log(front_img.style.top)
+
+window.onload = ()=>{
+   let id = setInterval(() => {
+    }, 10);
 }
-main2 = (pointer,num) =>{
-    pointer.style.display = "block"
-    nav_hover = document.querySelector(".nav-hover"+num)
-    nav_hover.style.display = "none"
-}
-
-
-
-
-
 
 
 
